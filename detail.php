5ab0b6199e0f6bc300add55914ed6480ccb876dd
Anne <annegeerkens@gmail.comgit credential-osxkeychaingit credential-osxkeychaincd ..cd desktopAnne:/ annegeerkens$ git config --global user.name AnneAnne:/ annegeerkens$ git config --global user.email annegeerkens@gmail.com>
2018-03-18 19:05:14 +0100

    inhoud detailpagina

--- detail.php
+++ detail.php
@@ -1,10 +1,30 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<meta charset="UTF-8">
+	 <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width">
+    <link rel="stylesheet" type="text/css" href="style/reset.css">
+    <link rel="stylesheet" type="text/css" href="style/style.css">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
 	<title>Phomo | Details</title>
 </head>
 <body>
-	
+  <?php include_once("nav.inc.php"); ?>
+  
+  <div id="detail_photo">
+      <div class="item clearfix">
+         <div class="user">
+              <img src="https://images.unsplash.com/profile-1501760727417-d777ab88d48b?dpr=2&auto=format&fit=crop&w=128&h=128&q=60&cs=tinysrgb&crop=faces&bg=fff" alt="avatar" class="avatar">
+              <a href="#">Joshua K. Jackson</a>
+         </div>
+         <a href="#"><img src="https://images.unsplash.com/photo-1519221584559-dea13724c873?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=b02ef53080a2d07699d854cf9a528c45&auto=format&fit=crop&w=934&q=80" alt="image" class="picture_index"></a>
+         
+         <div id="detail_photo_text">
+         <div class="date">17/03/2018</div>
+         <div class="likes">Likes</div>
+         <div class="comments"><strong>Username:</strong> Oh My God! Cool pic!</div>
+         </div>
+      </div>
+	</div>
 </body>
 </html>
