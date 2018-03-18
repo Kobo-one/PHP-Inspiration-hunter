878dcd4d23e046d6f0b4370fd2e06e8f43f6af5c
Anne <annegeerkens@gmail.comgit credential-osxkeychaingit credential-osxkeychaincd ..cd desktopAnne:/ annegeerkens$ git config --global user.name AnneAnne:/ annegeerkens$ git config --global user.email annegeerkens@gmail.com>
2018-03-18 15:39:39 +0100

    google fonts ingesloten

--- nav.inc.php
+++ nav.inc.php
@@ -1,12 +1,13 @@
 <nav class="navbar">
-           <a href="index.php">
-           <img src="images/logoklein_phomo.png" alt="Phomo logo" class="logonav">
-           </a>
-           
-           <a href="">Upload</a>
-           <a href="profile.php">Profile</a>
-           <a href="">Logout</a>
-           <form action="" method="get">
-           <input type="text" name="search" id="searchfield" placeholder="Search">
-           </form>
+    <a href="index.php">
+        <img src="images/logoklein_phomo.png" alt="Phomo logo" class="logonav">
+        <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+    </a>
+
+    <a href="upload.php">Upload</a>
+    <a href="profile.php">Profile</a>
+    <a href="">Logout</a>
+    <form action="" method="get">
+        <input type="text" name="search" id="searchfield" placeholder="Search">
+    </form>
 </nav>
