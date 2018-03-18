e82315c107efbdd2fe5359dfb5e4da1107eb74b8
Anne <annegeerkens@gmail.comgit credential-osxkeychaingit credential-osxkeychaincd ..cd desktopAnne:/ annegeerkens$ git config --global user.name AnneAnne:/ annegeerkens$ git config --global user.email annegeerkens@gmail.com>
2018-03-18 15:42:56 +0100

    upload pagina aangemaakt

--- upload.php
+++ upload.php
@@ -2,9 +2,25 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
+	 <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width">
+    <link rel="stylesheet" type="text/css" href="style/reset.css">
+    <link rel="stylesheet" type="text/css" href="style/style.css">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
 	<title>Phomo | Upload inspiration</title>
 </head>
 <body>
-	
+
+<?php include_once("nav.inc.php"); ?>
+
+<div id="upload_photo">
+
+
+<h1>Wanna share some inspiration?</h1>
+<div class="formfield">
+					<input type="submit" value="Camera" class="button">
+					<input type="submit" value="Library" class="button">	
+				</div>
+	</div>	
 </body>
 </html>
