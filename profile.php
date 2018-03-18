fc1618c97a47f9b01beaa7bfcb8b485c2187e065
Anne <annegeerkens@gmail.comgit credential-osxkeychaingit credential-osxkeychaincd ..cd desktopAnne:/ annegeerkens$ git config --global user.name AnneAnne:/ annegeerkens$ git config --global user.email annegeerkens@gmail.com>
2018-03-18 19:08:16 +0100

    comments bij de fotos gezet

--- profile.php
+++ profile.php
@@ -32,70 +32,99 @@
 	  <div class="item clearfix">
          
          <a href="#"><img src="https://images.unsplash.com/photo-1519221584559-dea13724c873?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=b02ef53080a2d07699d854cf9a528c45&auto=format&fit=crop&w=934&q=80" alt="image" class="picture_index"></a>
+         
+        <div id="detail_photo_text">
+         
          <div class="date">17/03/2018</div>
          <div class="likes">Likes</div>
+         <div class="comments"><strong>Username:</strong> Oh My God! Cool pic!</div>
+         
+         </div>
       </div>
       
       <div class="item clearfix">
          
           <a href="#"><img src="https://images.unsplash.com/photo-1515342870411-fdedf6a7c373?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=3f4e0f48166791d198e5cc2d9a8419b6&auto=format&fit=crop&w=962&q=80" alt="image" class="picture_index"></a>
-         <div>
-         <div class="date">16/03/2018</div>
+         <div id="detail_photo_text">
+         
+         <div class="date">17/03/2018</div>
          <div class="likes">Likes</div>
+         <div class="comments"><strong>Username:</strong> Oh My God! Cool pic!</div>
+         
          </div>
       </div>
       
       <div class="item clearfix">
        
           <a href="#"><img src="https://images.unsplash.com/photo-1494797262163-102fae527c62?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=0f06672063bb4021d19469dd62e5a1d9&auto=format&fit=crop&w=1000&q=80" alt="image" class="picture_index"></a>
-         <div>
+        <div id="detail_photo_text">
+         
          <div class="date">17/03/2018</div>
          <div class="likes">Likes</div>
+         <div class="comments"><strong>Username:</strong> Oh My God! Cool pic!</div>
+         
          </div>
       </div>
       
       <div class="item clearfix">
         
           <a href="#"><img src="https://images.unsplash.com/photo-1514908162061-89747fab8b1e?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=fb9e2cc4a906ae0c1685939dcb4c82a3&auto=format&fit=crop&w=2100&q=80" alt="image" class="picture_index"></a>
-         <div>
+         <div id="detail_photo_text">
+         
          <div class="date">17/03/2018</div>
          <div class="likes">Likes</div>
+         <div class="comments"><strong>Username:</strong> Oh My God! Cool pic!</div>
+         
          </div>
       </div>
       
       <div class="item clearfix">
          
           <a href="#"><img src="https://images.unsplash.com/photo-1512794751227-fefb9f898224?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=ed90dfd9d5890d4edd8a9288d940572e&auto=format&fit=crop&w=2100&q=80" alt="image" class="picture_index"></a>
-         <div>
-         <div class="date">15/03/2018</div>
+        <div id="detail_photo_text">
+         
+         <div class="date">17/03/2018</div>
          <div class="likes">Likes</div>
+         <div class="comments"><strong>Username:</strong> Oh My God! Cool pic!</div>
+         
          </div>
       </div>
       
       <div class="item clearfix">
         
           <a href="#"><img src="https://images.unsplash.com/photo-1516203294340-5ba5f612dc6a?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=1b79294f231ab4799218e82818a07de1&auto=format&fit=crop&w=1770&q=80" alt="image" class="picture_index"></a>
-         <div>
-         <div class="date">16/03/2018</div>
+        <div id="detail_photo_text">
+         
+         <div class="date">17/03/2018</div>
          <div class="likes">Likes</div>
+         <div class="comments"><strong>Username:</strong> Oh My God! Cool pic!</div>
+         
          </div>
       </div>
       
       <div class="item clearfix">
          
           <a href="#"><img src="https://images.unsplash.com/photo-1486570318579-054c95b01160?ixlib=rb-0.3.5&s=8cb4fb1b4ac3ab4e5335a6f5961d5d86&auto=format&fit=crop&w=2390&q=80" alt="image" class="picture_index"></a>
-         <div>
-         <div class="date">16/03/2018</div>
+         <div id="detail_photo_text">
+         
+         <div class="date">17/03/2018</div>
          <div class="likes">Likes</div>
+         <div class="comments"><strong>Username:</strong> Oh My God! Cool pic!</div>
+         
          </div>
       </div>
       
       <div class="item clearfix">
          
           <a href="#"><img src="https://images.unsplash.com/photo-1478001517127-fccc92f54906?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=63597be27c598539afd0becc211be037&auto=format&fit=crop&w=2100&q=80" alt="image" class="picture_index"></a>
-         <div>
-         <div class="date">15/03/2018</div>
+          
+          
+         <div id="detail_photo_text">
+         
+         <div class="date">17/03/2018</div>
          <div class="likes">Likes</div>
+         <div class="comments"><strong>Username:</strong> Oh My God! Cool pic!</div>
+         
          </div>
       </div>
 
