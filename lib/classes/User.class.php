18b79bdceb3970fd6276b366ab6c0a1e17f013c1
ilonaVS <37265476+ilonaVS@users.noreply.github.com>
2018-03-21 21:16:46 +0100

    Database class en user class

---
+++ lib/classes/User.class.php
@@ -0,0 +1,92 @@
+<?php
+
+    class User {
+        private $firstName;
+        private $lastName;
+        private $email;
+        private $password;
+        
+    /*Setters*/
+        
+    public function setFirstName($firstName)
+    {
+        if(empty($firstName)){
+            throw new Exception ("Please fill in your firstname.");
+        }
+            $this->firstName = $firstName;
+            return $this;
+    }
+        
+    public function setLastName($lastName)
+    {
+        if(empty($lastName)){
+            throw new Exception ("Please fill in your lastname.");
+        }
+        $this->lastName = $lastName;
+        return $this;
+    }   
+        
+    public function setEmail($email)
+    {
+      if(empty($email)){
+        throw new Exception ("Please fill in your e-mail.");
+      }
+        $this->email = $email;
+        return $this;
+    }
+
+    public function setPassword($password)
+    {
+        if(empty($password)){
+            throw new Exception ("Please fill in a password.");
+        }
+        $this->password = $password;
+        return $this;
+    }
+
+    /*Getters*/
+    
+    public function getFirstName()
+    {
+        return $this->firstName;
+    }
+        
+    public function getLastName()
+    {
+        return $this->lastName;
+    }
+        
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    public function register() 
+    {
+        $conn = new PDO('mysql:host=192.168.0.237:3306; dbname=phomo', 'phomo', 'ozvms79FYLNibblv');
+            
+        $statement = $conn->prepare("insert into user (firstname, lastname, email, password) values (:firstName, :lastName, :email, :password)");
+            
+        $hash = password_hash($this->password, PASSWORD_BCRYPT);
+        $statement->bindParam(":firstName", $this->firstName);
+        $statement->bindParam(":lastName", $this->lastName);
+        $statement->bindParam(":email", $this->email);
+        $statement->bindParam(":password", $hash);
+                
+        $result = $statement->execute();
+            
+        return $result;
+            
+    }
+        
+        
+        
+    }
+
+
+?>
