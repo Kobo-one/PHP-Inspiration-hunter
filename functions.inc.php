b8aff74d3a53c5edca34c5ccc575cdcf92a6616d
Anne <annegeerkens@gmail.comgit credential-osxkeychaingit credential-osxkeychaincd ..cd desktopAnne:/ annegeerkens$ git config --global user.name AnneAnne:/ annegeerkens$ git config --global user.email annegeerkens@gmail.com>
2018-03-26 14:48:47 +0200

    functions incl aangemaakt voor de functies van in te loggen apart te zetten

---
+++ functions.inc.php
@@ -0,0 +1,25 @@
+<?php
+
+    // moet ik hier nog een connectie maken dan? of is met include oke??
+   include_once("Db.class.php");
+
+//this function checks if a user can login and returns TRUE or FALSE
+function canILogin($Username, $Password){
+
+
+//BCRYPT METHODE
+//checken of je kan inloggen of niet (sessie aanmaken moet in andere functie)
+$query = "select * from users where email = '".$conn->real_escape_string($Username)."'";
+	$result = $conn->query($query);
+		if($result->num_rows == 1){
+			$user = $result->fetch_assoc();
+			if(password_verify($Password, $user['password'])){
+				return true;
+			}
+	}
+	else{
+		return false;
+	}
+}
+
+?>
