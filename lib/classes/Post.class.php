e9d3273e01a8c9316ec7bd896b3abc200ed0195b
Sara Breugelmans <sara_breugelmans@hotmail.com>
2018-03-27 11:42:19 +0200

    added Post class

---
+++ lib/classes/Post.class.php
@@ -0,0 +1,126 @@
+<?php
+include_once("Db.class.php");
+
+class Post{
+  private $image;
+  private $description;
+  private $userId;
+  private $created;
+  private $deleted;
+
+
+  /**
+   * Get the value of image
+   */ 
+  public function getImage()
+  {
+    return $this->image;
+  }
+
+  /**
+   * Set the value of image
+   *
+   * @return  self
+   */ 
+  public function setImage($image)
+  {
+    $this->image = $image;
+
+    return $this;
+  }
+
+  /**
+   * Get the value of description
+   */ 
+  public function getDescription()
+  {
+    return $this->description;
+  }
+
+  /**
+   * Set the value of description
+   *
+   * @return  self
+   */ 
+  public function setDescription($description)
+  {
+    $this->description = $description;
+
+    return $this;
+  }
+
+  /**
+   * Get the value of userId
+   */ 
+  public function getUserId()
+  {
+    return $this->userId;
+  }
+
+  /**
+   * Set the value of userId
+   *
+   * @return  self
+   */ 
+  public function setUserId($userId)
+  {
+    $this->userId = $userId;
+
+    return $this;
+  }
+
+  /**
+   * Get the value of created
+   */ 
+  public function getCreated()
+  {
+    return $this->created;
+  }
+
+  /**
+   * Set the value of created
+   *
+   * @return  self
+   */ 
+  public function setCreated($created)
+  {
+    $this->created = $created;
+
+    return $this;
+  }
+
+  /**
+   * Get the value of deleted
+   */ 
+  public function getDeleted()
+  {
+    return $this->deleted;
+  }
+
+  /**
+   * Set the value of deleted
+   *
+   * @return  self
+   */ 
+  public function setDeleted($deleted)
+  {
+    $this->deleted = $deleted;
+
+    return $this;
+  }
+
+
+
+  public static function getAll(){
+    $conn = Db::getInstance();
+    $statement= $conn->prepare('SELECT * FROM posts');
+    $statement->execute();
+    return $statement->fetchAll(PDO::FETCH_ASSOC);
+
+  }
+}
+
+
+
+
+ ?>
