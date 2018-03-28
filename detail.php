0d1515b3fa6381d7c1f1834380bf31c038723132
Sara Breugelmans <sara_breugelmans@hotmail.com>
2018-03-28 21:14:22 +0200

    detail.php via db

--- detail.php
+++ detail.php
@@ -2,12 +2,11 @@
 include_once("lib/classes/Post.class.php");
 
 $post = new Post();
-$id=$post->setSearch($_GET['post']);
+$id=$_GET['post'];
+$post->setSearch($id);
 
 $collection= $post->getDetailsPost();
-var_dump($collection);
-/*"1" ["image"]=> string(168) "https://images.unsplash.com/photo-1519221584559-dea13724c873?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=b02ef53080a2d07699d854cf9a528c45&auto=format&fit=crop&w=934&q=80" ["description"]=> string(8) "Mooimooi" ["post_user_id"]=> string(1) "1" ["created"]=> string(19) "2018-03-27 02:00:00" ["deleted"]=> string(1) "" ["firstname"]=> string(4) "Kobe" ["lastname"]=> string(13) "Christiaensen" ["picture"]=> string(60) "https://netasite.net/wp-content/uploads/2013/05/90d95000.jpg" } } 
-    */
+
 
 ?><!DOCTYPE html>
 <html lang="en">
@@ -22,18 +21,18 @@ var_dump($collection);
 </head>
 <body>
   <?php include_once("nav.inc.php"); ?>
-  
+  <?php foreach($collection as $key =>$c): ?> 
   <div id="detail_photo">
       <div class="item clearfix">
          <div class="user">
               <img src="<?php echo $c['picture'];?>" alt="avatar" class="avatar">
-              <a href="profile.php?user=<?php echo $id;?>"><?php var_dump( $collection[$id]['firstname']). " ". $collection['lastname']?></a>
+              <a href="profile.php?user=<?php echo $id;?>"><?php echo( $c['firstname']). " ". $c['lastname']?></a>
          </div>
-<?php foreach($collection as $key =>$c): ?>      
+     
          <a href="#"><img src="<?php echo $c['image'];?>" alt="image" class="picture_index"></a>
          
          <div id="detail_photo_text">
-         <div class="date"><?php ?></div>
+         <div class="date"><?php echo $c['created'] ?></div>
          <div class="likes">Likes</div>
          <div class="comments"><strong>Username:</strong> Oh My God! Cool pic!</div>
          </div>
