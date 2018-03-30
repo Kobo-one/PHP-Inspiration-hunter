cf522ee66be88749f547ecd2ee1984c75f0c401e
Sara Breugelmans <sara_breugelmans@hotmail.com>
2018-03-30 12:20:35 +0200

    feature2: finished

--- login.php
+++ login.php
@@ -2,6 +2,7 @@
     // zijn al die includes nodig of enkel function??
     include_once("lib/classes/User.class.php");
     include_once("lib/helpers/Security.class.php");
+    session_start();
     if(isset($_SESSION["username"])){
       header("Location: index.php");
     }
