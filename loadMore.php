1abdd90d0b7d7049863859f13d69acffae6f94ff
Sara Breugelmans <sara_breugelmans@hotmail.com>
2018-03-31 18:21:30 +0200

    added some comments

--- loadMore.php
+++ loadMore.php
@@ -1,25 +1,7 @@
 <?php
   
   include_once("lib/classes/Post.class.php");
-   //checken of ajax call leeg is of niet(niet het form)
-    /*
-        //$.ajax meegegeven (niet uit de form)
-        $comment=$_POST['comment'];
-        $postId=$_POST['postId'];
-
-        $db= Db::getInstance();
-        $c= new Comment($db);
-        $c->setComment($comment);
-        $c->setPostId($postId);
-        $c->Save();
-
-        $response= [
-            "status" => "succes",
-            "comment" => $comment
-        ];
-        header('Content-Type: application/json');
-        echo json_encode($response);
-    }  */
+   
     if(!empty($_POST)){
     $i=$_POST['i'];
    
