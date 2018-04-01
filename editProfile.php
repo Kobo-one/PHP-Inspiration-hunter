deac4e9ac5eeb69565af63028f9d671b2acae26f
ilonaVS <37265476+ilonaVS@users.noreply.github.com>
2018-04-01 15:02:55 +0200

    editProfile CSS

--- editProfile.php
+++ editProfile.php
@@ -16,20 +16,20 @@ include_once("lib/includes/checklogin.inc.php");
 <body>   
    <?php include_once("nav.inc.php"); ?>
    <div class="profile_settings">
-   <h1>Change your settings</h1>
+   <h1>Profile settings</h1>
    <img src="https://images.unsplash.com/profile-fb-1501021622-8b449a1a8dd6.jpg?dpr=2&auto=format&fit=crop&w=128&h=128&q=60&cs=tinysrgb&crop=faces&bg=fff" alt="avatar" class="edit_avatar">
    
    <form action="" method="post" enctype="multipart/form-data">
        <div class="formfield" id="first_input">
-            <label for="image_upload" class="button_upload" id="choose_image">New profile pic</label>
+            <label for="image_upload" class="button_upload" id="choose_image">Edit profile pic</label>
             <input type="file" name="image" id="image_upload" accept=".jpg, .jpeg, .png">
         </div>	    
         <div class="formfield">
-            <label for="username" class="profile_label">Current Username</label>
+            <label for="username" class="profile_label">Username</label>
 			<input type="text" id="username" name="username" placeholder="Change your username">
         </div>
 	    <div class="formfield">
-	        <label for="email" class="profile_label">Current E-mail</label>
+	        <label for="email" class="profile_label">E-mail</label>
 			<input type="text" id="email" name="email" placeholder="Change your e-mail">
 		</div>
         <div class="formfield">
