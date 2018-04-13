fa98b31d5cc26ad1c6411b9396b382273a8d85cd
Sara Breugelmans <sara_breugelmans@hotmail.com>
2018-04-13 17:07:00 +0200

    datetimetimeAgo function in index.php

--- index.php
+++ index.php
@@ -1,8 +1,46 @@
 <?php
+
 include_once("lib/classes/Post.class.php");
 include_once("lib/includes/checklogin.inc.php");
 $collection= Post::getAll();
 $counter=0;
+/*object(DateInterval)#11 (16) {
+     ["y"]=> int(0)
+      ["m"]=> int(0)
+       ["d"]=> int(14) 
+       ["h"]=> int(3) 
+       ["i"]=> int(10) 
+       ["s"]=> int(17) ["f"]=> float(0.429323) ["weekday"]=> int(0) ["weekday_behavior"]=> int(0) ["first_last_day_of"]=> int(0) ["invert"]=> int(1) ["days"]=> int(14) ["special_type"]=> int(0) ["special_amount"]=> int(0) ["have_weekday_relative"]=> int(0) ["have_special_relative"]=> int(0) } 14 day(s) ago */
+
+function timeAgo($pTime){
+// tijdzone veranderen naar brusselse
+    date_default_timezone_set("Europe/Brussels");
+    
+$postTime = new DateTime($pTime);
+$currentTime = new DateTime();
+
+//verschil tussen timestamp post en current time
+$interval = $currentTime->diff($postTime);
+
+
+if ($interval->h==0  && $interval->d==0 && $interval->m==0){
+    return $interval->format('%i minute(s) ago')."\n";
+}
+if ($interval->d==0 && $interval->m==0 && $interval->h>=1){
+    return $interval->format('%h hour(s) ago')."\n";
+}
+if($interval->m==0 && $interval->y==0){
+    return $interval->format('%a day(s) ago')."\n";
+}
+if($interval->y==0){
+    return $interval->format('%m month(s), %d day(s) ago');
+}
+if($interval->y>=1){
+    return $interval->format('%y year(s), %m month(s) ago');
+}
+
+}
+
 ?><!DOCTYPE html>
 <html lang="en">
 <head>
@@ -36,7 +74,7 @@ $counter=0;
          </div>
          <a href="detail.php?post=<?php echo $c['id'] ?>"><img src="<?php echo $c['image']; ?> " alt="image" class="picture_index"></a>
          <div class="feed_flex">
-         <div class="date"><?php echo $c['created'];?></div>
+         <div class="date"><?php echo(timeAgo($c['created']));?></div>
          <div class="likes"># likes</div>
          <a href="#"><img src="images/tolike_btn.png" alt="like button" class="like_btn"></a>
          </div>
