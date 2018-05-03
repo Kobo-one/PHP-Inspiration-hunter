22c0c19973ef4112422b416a5ce6cba64b32e030
Sara Breugelmans <sara_breugelmans@hotmail.com>
2018-05-03 18:19:12 +0200

    createComment met ajax

--- lib/ajax/ajax_createcomment.php
+++ lib/ajax/ajax_createcomment.php
@@ -1,29 +1,32 @@
 <?php	
 		include_once("../classes/Comment.class.php");
-        include_once("../classes/User.class.php");
-        include_once("../classes/Post.class.php");
-
-		$comment = new Comment();
+      
+		if(!empty($_POST)){
+			session_start();
+			$comment = new Comment();
         
-        try
-		{
+        
             //hierbij zetten we niet de name tussen de [], maar de naam 'comment' die we aan de data hebben gegeven in app.js
             $comment->setText( $_POST['comment'] );
-            //postId uithalen
-            $comment->setPostId($_GET['post']);
-            $comment->saveComment(); 
-            $feedback['text'] = "Your comment has been posted!";
+			//postId uithalen
+			
+            $comment->setPostId($_POST['postId']);
+			$comment->saveComment(); 
+			
+            /*$feedback['text'] = "Your comment has been posted!";
 			$feedback['status'] = "success";
-            $feedback['comment'] = htmlspecialchars( $_POST['comment'] );
- 
-		}
-		catch(Exception $e)
-		{
-			$feedback['text'] = $e->getMessage();
-			$feedback['status'] = "error";
+            $feedback['comment'] = htmlspecialchars( $_POST['comment'] );*/
+			$feedback= [
+				"status" => "success",
+				"comment"=> htmlspecialchars( $_POST['comment'] ),
+				"user"=>$_SESSION['username']
+				
+			];
+			header('Content-Type: application/json');
+			echo json_encode($feedback);
+			
 		}
 
-		header('Content-Type: application/json');
-		echo json_encode($feedback);
+		
 
 ?>
