44efaf56448b31c57401cdb4110dc35d78ef3d77
Sara Breugelmans <sara_breugelmans@hotmail.com>
2018-05-03 14:20:01 +0200

    gitignore added

--- lib/classes/Db.class.php
+++ lib/classes/Db.class.php
@@ -9,7 +9,8 @@
         ->if !exists ->return new PDO
         */
         public static function getInstance(){
-            include_once($_SERVER['DOCUMENT_ROOT']."/lib/settings/db.php");
+           
+            include_once($_SERVER["DOCUMENT_ROOT"]."/PHOMO/PHP-Inspiration-hunter/lib/settings/db.php");
             
             if( self::$conn == null ){
                 self::$conn = new PDO('mysql:host='.$db['host'].'; dbname='.$db['dbname'].'', $db['username'], $db['password']);
